089d42c23a9d832c834fdab27695f56e4cd4a5a8
Stelios Ntoumas <steliosntoumas@outlook.com>
2025-06-19 15:30:53 +0300

    implement navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "@/components/NavItems";
+
+const Navbar = () => {
+    return (<nav className="navbar">
+        <Link href="/">
+            <div className="flex items-center gap-2.5 cursor-pointer">
+                <Image src="/images/logo.png" alt="logo" width={100} height={100} className="object-cover invert"/>
+            </div>
+        </Link>
+        <div className="flex items-center gap-8">
+            <NavItems/>
+            <p>Sign In</p>
+        </div>
+    </nav>)
+}
+export default Navbar
